b73590089a789c058f59b7f08ec5b3a43e78634e
Vedang <vedkapse18@gmail.com>
2022-01-27 14:21:24 -0800

    initial commit

---
+++ testapp/src/components/App.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+    console.log(contact);
+  }
+  return (
+    <div className="ui container">
+      <Header/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts}/>
+    </div>
+    );
+}
+
+export default App;
